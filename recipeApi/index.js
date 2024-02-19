dfdf12a2677a772365815f83927f66730b127e1b
nexox <leoley.lange@gmail.com>
2024-02-19 08:31:32 +0100

    folder structure

---
+++ recipeApi/index.js
@@ -0,0 +1,34 @@
+const express = require('express');
+const app = express();
+const Port = 3000;
+ 
+app.use(express.json());
+ 
+app.listen(
+    Port,
+    () => console.log(`It's alive on http://localhost:${Port}`)
+)
+ 
+app.get('/recipe', (req, res) => {
+    res.status(200).send({
+        dish: 'Pizza Magaritha',
+        country: 'Italy'
+    })
+});
+ 
+app.post('/recipe/:id', (req, res) => {
+    const { id } = req.params;
+    const { name } = req.body;
+ 
+    if (!name) {
+        return res.status(418).send({ message: 'We need a logo' });
+    }
+ 
+    if (res.headersSent) {
+        console.error('Headers already sent');
+    } else {
+        res.send({
+            dish: `with your ${name} and ID of ${id}`,
+        });
+    }
+});
