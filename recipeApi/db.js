85d351f5c805a64b6c4e684d5fcdb5b917b15e61
leonie <leoley.lange@gmail.com>
2024-02-29 08:08:46 +0000

    api container running

--- recipeApi/db.js
+++ recipeApi/db.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 
 function loadDb(){
-  mongoose.connect('mongodb://root:example@10.115.1.14:27017', {
+  mongoose.connect('mongodb://root:example@mongo_container:27017', {
     useNewUrlParser: true,
     useUnifiedTopology: true,
   }).then(() => {
