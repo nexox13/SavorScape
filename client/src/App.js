77fd42ece603c92d68ef27682ddb87250e3a88ee
leonie <leoley.lange@gmail.com>
2024-03-15 09:30:10 +0000

    pointer on icons

--- client/src/App.js
+++ client/src/App.js
@@ -2,12 +2,11 @@ import React from 'react';
 import Header from './components/Header';
 import MainContent from './components/MainContent';
 import { AppContextProvider } from './contexts/AppContext';
-import RecipeValidation from './validation/inputRecipe'
+//import RecipeValidation from './validation/inputRecipe'
 
 function App() {
   return (
     <AppContextProvider>
-      <RecipeValidation/>
       <div className="min-h-screen max-h-screen overflow-hidden">
         <Header className="sticky top-0 left-0 w-auto p-4 opacity-75 z-50"/>
 
