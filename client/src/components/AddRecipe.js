889df132617538185861eee1cd922364cc05e3be
leonie <leoley.lange@gmail.com>
2024-03-21 08:32:22 +0000

    add recipe

--- client/src/components/AddRecipe.js
+++ client/src/components/AddRecipe.js
@@ -1,20 +1,71 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react';
+import '../index.css'
 
 const getRecipes = async() => {
-    const url = `http://10.115.1.14:3001/api/recipes`
-    
-    const response = await fetch(url)
-    const responseJson = await response.json()
-    console.log(responseJson)
-  };
+  const url = `http://10.115.1.14:3001/api/recipes`
+  
+  const response = await fetch(url)
+  const responseJson = await response.json()
+  console.log(responseJson)
+};
 
-  getRecipes()
+getRecipes()
 
 function AddRecipe() {
+  const [name, setName] = useState('');
+  const [country, setCountry] = useState('');
+  const [ingredients, setIngredients] = useState('');
+  const [instructions, setInstructions] = useState('');
+
+  const handleChange = (type, value) => {
+    switch (type) {
+      case 'name':
+        setName(value);
+        break;
+      case 'country':
+        setCountry(value);
+        break;
+      case 'ingredients':
+        setIngredients(value);
+        break;
+      case 'instructions':
+        setInstructions(value);
+        break;
+      default:
+        break;
+    }
+  };
+
   return (
-    <div>
-      Hallo
-    </div>
+    <span style={{ opacity: 1, backgroundColor:'white', fontWeight: 'bold' }}>
+      <div className="overflow-auto bg-gray-100 p-4 rounded-xl shadow-md h-auto">
+        <h2 className="text-xl font-bold mb-4 justify-center">Add a Recipe</h2>
+        <input
+          className="opacity-100 my-1.5 w-full h-1/4 px-5 bg-violet-600 min-h-10 max-h-96 p-2 border border-gray-300 rounded-full focus:outline-none focus:ring focus:border-blue-500"
+          value={name}
+          onChange={(e) => handleChange('name', e.target.value)}
+          placeholder="Recipe name"
+        />
+        <input
+          className="opacity-100 my-1.5 w-full h-1/4 px-5 bg-violet-600 min-h-10 max-h-96 p-2 border border-gray-300 rounded-full focus:outline-none focus:ring focus:border-blue-500"
+          value={country}
+          onChange={(e) => handleChange('country', e.target.value)}
+          placeholder="Recipe country"
+        />
+        <textarea
+          className="opacity-100 h-80 my-1.5 w-full h-1/4 px-5 bg-violet-600 min-h-10 p-2 border border-gray-300 rounded-3xl focus:outline-none focus:ring focus:border-blue-500"
+          value={ingredients}
+          onChange={(e) => handleChange('ingredients', e.target.value)}
+          placeholder="Recipe ingredients"
+        />
+        <textarea
+          className="opacity-100 my-1.5 w-full h-80 px-5 bg-violet-600 p-2 border border-gray-300 rounded-3xl focus:outline-none focus:ring focus:border-blue-500"
+          value={instructions}
+          onChange={(e) => handleChange('instructions', e.target.value)}
+          placeholder="Recipe instructions"
+        />
+      </div>
+    </span>
   )
 }
 
--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -5,7 +5,18 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      borderRadius: {
+        'none': '0',
+        'sm': '0.125rem',
+        DEFAULT: '0.25rem',
+        DEFAULT: '4px',
+        'md': '0.375rem',
+        'lg': '0.5rem',
+        'full': '9999px',
+        'large': '30px',
+      }
+    },
   },
   plugins: [],
 }
