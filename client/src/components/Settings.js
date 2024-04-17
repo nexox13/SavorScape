746fde13f3673c1e091dac16f43c833f0e1a1cfb
Stefan Haller <94039487+MthSquezz@users.noreply.github.com>
2024-04-17 10:25:37 +0200

    Font-Style

--- client/src/components/Settings.js
+++ client/src/components/Settings.js
@@ -16,7 +16,7 @@ function Settings({ mapTheme, setMapTheme }) {
 
   return (
     <div className="flex flex-col space-y-4 p-4 bg-white rounded-lg shadow-md">
-      <h2 className="text-xl font-bold flex justify-center">Settings</h2>
+      <h2 className="text-2xl font-bold mb-4 font-mono flex justify-center">Settings</h2>
       <div>
         <h2 className="text-xl font-semibold">Theme:</h2>
       </div>
