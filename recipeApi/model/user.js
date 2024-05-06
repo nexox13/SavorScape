e16f40a8a1a85567a7ee620a261cacd311ce1f0b
leonie <leoley.lange@gmail.com>
2024-05-06 06:59:40 +0000

    Added Bcryp

--- recipeApi/model/user.js
+++ recipeApi/model/user.js
@@ -1,8 +1,15 @@
 const mongoose = require('mongoose');
 
 const UserSchema = mongoose.Schema({
-    firstName: String,
-    lastName: String
+    username: {
+        type: String,
+        required: true,
+        unique: true,
+      },
+    password: {
+        type: String,
+        required: true,
+      },
 }, {
     timestamps: true
 });
