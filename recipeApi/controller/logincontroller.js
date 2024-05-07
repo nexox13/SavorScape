8e8f21603e76d8576a3cbfa9e5c595342450c498
leonie <leoley.lange@gmail.com>
2024-05-07 13:48:05 +0000

    Login routes included and Webtoken generate

--- recipeApi/controller/logincontroller.js
+++ recipeApi/controller/logincontroller.js
@@ -1,3 +1,30 @@
 const express = require('express');
 const User = require('../model/user.js');
 const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+
+var router = express.Router();
+
+//login
+router.post('/', async (req, res) => {
+    const user = await User.findOne({ username: req.body.username });
+
+    const userAllowed = await bcrypt.compare(req.body.password, user.password);
+    console.log(userAllowed)
+
+    if (userAllowed) {
+        const userObject = user.toObject();
+        const payload = {
+            id: userObject._id,
+            username: userObject.username,
+            password: userObject.password
+        };
+        const token = jwt.sign(payload, 'secret-key-shhhh');
+        console.log(token);
+        res.status(200).json({ message: 'User logged in successfully', token });
+    } else {
+        res.send('No user found or invalid password');
+    }
+});
+
+module.exports = router;
