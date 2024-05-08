7328367de95ce65d18f6210c17c9cf7c6c78320d
MthSquezz <94039487+MthSquezz@users.noreply.github.com>
2024-05-08 16:39:41 +0200

    refactor: Update AppContext to use destructuring for useState imports
    This commit refactors the import statement in the AppContext file to use destructuring for the useState function from the react module. This improves the readability of the code and follows best practices for importing functions from modules.

--- client/src/components/UserSettings.js
+++ client/src/components/UserSettings.js
@@ -1,118 +1,203 @@
-  import React, { useState } from 'react';
-  import { useAppContext } from '../contexts/AppContext';
+import React, { useState } from "react";
+import { useAppContext } from "../contexts/AppContext";
 
-  function UserSettings() {
-      const { colorTheme, settingsUsername, setIsRegistering, setSettingsUsername, settingsUserPassword, setSettingsUserPassword, handleDeleteAccount, setLoggedIn } = useAppContext();
+function UserSettings() {
+  const {
+    colorTheme,
+    settingsUsername,
+    setIsRegistering,
+    setSettingsUsername,
+    settingsUserPassword,
+    setSettingsUserPassword,
+    handleDeleteAccount,
+    setLoggedIn,
+  } = useAppContext();
 
-      const[editUsername, setEditUsername] = useState('Edit')
-      const[editPassword, setEditPassword] = useState('Edit')
-      const[editUsernameState, setEditUsernameState] = useState(false)
-      const[editPasswordState, setEditPasswordState] = useState(false)
+  const [editUsernameState, setEditUsernameState] = useState(false);
+  const [editPasswordState, setEditPasswordState] = useState(false);
 
-      const handleLogOut = () => {
-        setLoggedIn(false);
-        setSettingsUsername('');
-        setSettingsUserPassword('');
-        setIsRegistering(false);
-        console.log("Logged Out ")
-      };
+  const handleLogOut = () => {
+    setLoggedIn(false);
+    setSettingsUsername("");
+    setSettingsUserPassword("");
+    setIsRegistering(false);
+    console.log("Logged Out ");
+  };
 
-      const toggleEditUsername = () => {
-        setEditUsernameState(!editUsernameState);
-        console.log("EditUsername")
-      };
-      const handleUsernameChange = (event) => {
-        setSettingsUsername(event.target.value);
-      };
-      
+  const toggleEditUsername = () => {
+    setEditUsernameState(!editUsernameState);
+  };
 
-      return (
-        <div className="bg-white mt-4 h-auto rounded-xl p-4">
-          <header className="font-mono text-xl font-bold mb-4">Account Settings:</header>
-          <section className="mt-4">
-            {/* Username */}
-            <div className="border-b-2 border-gray-300 pb-2 mb-4 flex justify-between items-center mr-6">
-              <div className="text-lg font-semibold">Username:</div>
+  const toggleEditPassword = () => {
+    setEditPasswordState(!editPasswordState);
+  };
 
-              {editUsernameState ? (
-                <input
-                  type="text"
-                  value={settingsUsername}
-                  onChange={handleUsernameChange}
-                  className="border border-black pr-1 pl-1 rounded"
-                />
-              ) : (
-                <div className="border border-black pr-1 pl-1 rounded">{settingsUsername}</div>
-              )}
-              
-              <div className="flex items-center">
-                  <div className="ml-2">
-                  <svg
-                      stroke="currentColor"
-                      fill="currentColor"
-                      strokeWidth="0"
-                      viewBox="0 0 16 16"
-                      height="1.3em"
-                      width="1.3em"
-                      xmlns="http://www.w3.org/2000/svg"
-                      onClick={toggleEditUsername}
-                  >
-                      <path
-                      fillRule="evenodd"
-                      d="M11.293 1.293a1 1 0 011.414 0l2 2a1 1 0 010 1.414l-9 9a1 1 0 01-.39.242l-3 1a1 1 0 01-1.266-1.265l1-3a1 1 0 01.242-.391l9-9zM12 2l2 2-9 9-3 1 1-3 9-9z"
-                      clipRule="evenodd"
-                      ></path>
-                      <path
-                      fillRule="evenodd"
-                      d="M12.146 6.354l-2.5-2.5.708-.708 2.5 2.5-.707.708zM3 10v.5a.5.5 0 00.5.5H4v.5a.5.5 0 00.5.5H5v.5a.5.5 0 00.5.5H6v-1.5a.5.5 0 00-.5-.5H5v-.5a.5.5 0 00-.5-.5H3z"
-                      clipRule="evenodd"
-                      ></path>
-                  </svg>
-                  </div>
+  const handleUsernameChange = (event) => {
+    setSettingsUsername(event.target.value);
+    // Fetch auf API-PUT
+  };
+
+  const handlePasswordChange = (event) => {
+    setSettingsUserPassword(event.target.value);
+    // Fetch auf API-PUT
+  };
+
+  return (
+    <div className="bg-white mt-4 h-auto rounded-xl p-4">
+      <header className="font-mono text-xl font-bold mb-4">
+        Account Settings:
+      </header>
+      <section className="mt-4">
+        {/* Username */}
+        <div className="border-b-2 border-gray-300 pb-2 mb-4 flex justify-between items-center mr-6">
+          <div className="text-lg font-semibold">Username:</div>
+
+          {editUsernameState ? (
+            <input
+              type="text"
+              value={settingsUsername}
+              onChange={handleUsernameChange}
+              className="border border-black pr-1 pl-1 rounded w-auto max-w-40"
+            />
+          ) : (
+            <div className="border border-black pr-1 pl-1 rounded">
+              {settingsUsername}
+            </div>
+          )}
+
+          {editUsernameState ? (
+            <div className="flex items-center">
+              <div className="ml-2">
+                <svg
+                  // Save
+                  stroke="currentColor"
+                  fill="currentColor"
+                  strokeWidth="0"
+                  viewBox="0 0 1024 1024"
+                  height="1.3em"
+                  width="1.3em"
+                  xmlns="http://www.w3.org/2000/svg"
+                  onClick={toggleEditUsername}
+                  className="cursor-pointer"
+                >
+                  <path d="M699 353h-46.9c-10.2 0-19.9 4.9-25.9 13.3L469 584.3l-71.2-98.8c-6-8.3-15.6-13.3-25.9-13.3H325c-6.5 0-10.3 7.4-6.5 12.7l124.6 172.8a31.8 31.8 0 0 0 51.7 0l210.6-292c3.9-5.3.1-12.7-6.4-12.7z"></path>
+                  <path d="M512 64C264.6 64 64 264.6 64 512s200.6 448 448 448 448-200.6 448-448S759.4 64 512 64zm0 820c-205.4 0-372-166.6-372-372s166.6-372 372-372 372 166.6 372 372-166.6 372-372 372z"></path>
+                </svg>
               </div>
             </div>
-
-            
-            {/* Password */}
-            <div className="border-b-2 border-gray-300 pb-2 mb-4 mr-6 flex justify-between items-center">
-              <div className="text-lg font-semibold">Password:</div>
-              <div className='border border-black pr-1 pl-1 rounded'>{settingsUserPassword}</div>
-              <div className="flex items-center">
-                  <div className="ml-2">
-                  <svg
-                      stroke="currentColor"
-                      fill="currentColor"
-                      strokeWidth="0"
-                      viewBox="0 0 16 16"
-                      height="1.3em"
-                      width="1.3em"
-                      xmlns="http://www.w3.org/2000/svg"
-                  >
-                      <path
-                      fillRule="evenodd"
-                      d="M11.293 1.293a1 1 0 011.414 0l2 2a1 1 0 010 1.414l-9 9a1 1 0 01-.39.242l-3 1a1 1 0 01-1.266-1.265l1-3a1 1 0 01.242-.391l9-9zM12 2l2 2-9 9-3 1 1-3 9-9z"
-                      clipRule="evenodd"
-                      ></path>
-                      <path
-                      fillRule="evenodd"
-                      d="M12.146 6.354l-2.5-2.5.708-.708 2.5 2.5-.707.708zM3 10v.5a.5.5 0 00.5.5H4v.5a.5.5 0 00.5.5H5v.5a.5.5 0 00.5.5H6v-1.5a.5.5 0 00-.5-.5H5v-.5a.5.5 0 00-.5-.5H3z"
-                      clipRule="evenodd"
-                      ></path>
-                  </svg>
-                  </div>
+          ) : (
+            <div className="flex items-center">
+              <div className="ml-2">
+                <svg
+                  // Edit
+                  stroke="currentColor"
+                  fill="currentColor"
+                  strokeWidth="0"
+                  viewBox="0 0 16 16"
+                  height="1.3em"
+                  width="1.3em"
+                  xmlns="http://www.w3.org/2000/svg"
+                  onClick={toggleEditUsername}
+                  className="cursor-pointer"
+                >
+                  <path
+                    fillRule="evenodd"
+                    d="M11.293 1.293a1 1 0 011.414 0l2 2a1 1 0 010 1.414l-9 9a1 1 0 01-.39.242l-3 1a1 1 0 01-1.266-1.265l1-3a1 1 0 01.242-.391l9-9zM12 2l2 2-9 9-3 1 1-3 9-9z"
+                    clipRule="evenodd"
+                  ></path>
+                  <path
+                    fillRule="evenodd"
+                    d="M12.146 6.354l-2.5-2.5.708-.708 2.5 2.5-.707.708zM3 10v.5a.5.5 0 00.5.5H4v.5a.5.5 0 00.5.5H5v.5a.5.5 0 00.5.5H6v-1.5a.5.5 0 00-.5-.5H5v-.5a.5.5 0 00-.5-.5H3z"
+                    clipRule="evenodd"
+                  ></path>
+                </svg>
               </div>
-            </div>  
-            
-            {/* Delete Account */}
-            <div className={`mt-4 underline text-black font-mono font-bold cursor-pointer rounded-xl border border-black p-2 w-3/5 flex justify-center ${colorTheme}`} onClick={handleDeleteAccount}>
-              Delete Account
             </div>
-          </section>
-          <div className={`mt-4 underline text-black font-mono font-bold cursor-pointer rounded-xl border border-black p-2 w-3/5 flex justify-center ${colorTheme}`} onClick={handleLogOut}>
-            Log Out
-          </div>
+          )}
         </div>
-      );
-  }
 
-  export default UserSettings;
+        {/* Password */}
+        <div className="border-b-2 border-gray-300 pb-2 mb-4 mr-6 flex justify-between items-center">
+          <div className="text-lg font-semibold">Password:</div>
+          {editPasswordState ? (
+            <input
+              type="password"
+              value={settingsUserPassword}
+              onChange={handlePasswordChange}
+              className="border border-black pr-1 pl-1 rounded w-auto max-w-40"
+            />
+          ) : (
+            <div className="border border-black pr-1 pl-1 rounded">
+              {settingsUserPassword}
+            </div>
+          )}
+
+          {editPasswordState ? (
+            <div className="flex items-center">
+              <div className="ml-2">
+                <svg
+                  // Save
+                  stroke="currentColor"
+                  fill="currentColor"
+                  strokeWidth="0"
+                  viewBox="0 0 1024 1024"
+                  height="1.3em"
+                  width="1.3em"
+                  xmlns="http://www.w3.org/2000/svg"
+                  onClick={toggleEditPassword}
+                  className="cursor-pointer"
+                >
+                  <path d="M699 353h-46.9c-10.2 0-19.9 4.9-25.9 13.3L469 584.3l-71.2-98.8c-6-8.3-15.6-13.3-25.9-13.3H325c-6.5 0-10.3 7.4-6.5 12.7l124.6 172.8a31.8 31.8 0 0 0 51.7 0l210.6-292c3.9-5.3.1-12.7-6.4-12.7z"></path>
+                  <path d="M512 64C264.6 64 64 264.6 64 512s200.6 448 448 448 448-200.6 448-448S759.4 64 512 64zm0 820c-205.4 0-372-166.6-372-372s166.6-372 372-372 372 166.6 372 372-166.6 372-372 372z"></path>
+                </svg>
+              </div>
+            </div>
+          ) : (
+            <div className="flex items-center">
+              <div className="ml-2">
+                <svg
+                  stroke="currentColor"
+                  fill="currentColor"
+                  strokeWidth="0"
+                  viewBox="0 0 16 16"
+                  height="1.3em"
+                  width="1.3em"
+                  xmlns="http://www.w3.org/2000/svg"
+                  onClick={toggleEditPassword}
+                  className="cursor-pointer"
+                >
+                  <path
+                    fillRule="evenodd"
+                    d="M11.293 1.293a1 1 0 011.414 0l2 2a1 1 0 010 1.414l-9 9a1 1 0 01-.39.242l-3 1a1 1 0 01-1.266-1.265l1-3a1 1 0 01.242-.391l9-9zM12 2l2 2-9 9-3 1 1-3 9-9z"
+                    clipRule="evenodd"
+                  ></path>
+                  <path
+                    fillRule="evenodd"
+                    d="M12.146 6.354l-2.5-2.5.708-.708 2.5 2.5-.707.708zM3 10v.5a.5.5 0 00.5.5H4v.5a.5.5 0 00.5.5H5v.5a.5.5 0 00.5.5H6v-1.5a.5.5 0 00-.5-.5H5v-.5a.5.5 0 00-.5-.5H3z"
+                    clipRule="evenodd"
+                  ></path>
+                </svg>
+              </div>
+            </div>
+          )}
+        </div>
+
+        {/* Delete Account */}
+        <div
+          className={`mt-4 underline text-black font-mono font-bold cursor-pointer rounded-xl border border-black p-2 w-3/5 flex justify-center ${colorTheme}`}
+          onClick={handleDeleteAccount}
+        >
+          Delete Account
+        </div>
+      </section>
+      <div
+        className={`mt-4 underline text-black font-mono font-bold cursor-pointer rounded-xl border border-black p-2 w-3/5 flex justify-center ${colorTheme}`}
+        onClick={handleLogOut}
+      >
+        Log Out
+      </div>
+    </div>
+  );
+}
+
+export default UserSettings;
--- client/src/contexts/AppContext.js
+++ client/src/contexts/AppContext.js
@@ -1,80 +1,73 @@
-import {
-    createContext,
-    useContext,
-    useState
-} from "react";
+import { createContext, useContext, useState } from "react";
 
 const AppContext = createContext();
 
-export const AppContextProvider = ({
-    children
-}) => {
-    const [showLogin, setShowLogin] = useState(false);
-    const [showAddRecipe, setShowAddRecipe] = useState(false);
-    const [showSettings, setShowSettings] = useState(false);
-    const [showNotepad, setShowNotepad] = useState(false);
+export const AppContextProvider = ({ children }) => {
+  const [showLogin, setShowLogin] = useState(false);
+  const [showAddRecipe, setShowAddRecipe] = useState(false);
+  const [showSettings, setShowSettings] = useState(false);
+  const [showNotepad, setShowNotepad] = useState(false);
 
-    const [selectedCountry, setSelectedCountry] = useState('');
-    const [searchedRecipe, setSearchedRecipe] = useState('')
-    const [searchSubmit, setSearchSubmit] = useState(false);
+  const [selectedCountry, setSelectedCountry] = useState("");
+  const [searchedRecipe, setSearchedRecipe] = useState("");
+  const [searchSubmit, setSearchSubmit] = useState(false);
 
-    const [mapTheme, setMapTheme] = useState(localStorage.getItem('mapTheme') || 'mapbox://styles/mapbox/dark-v11');
-    const [colorTheme, setColorTheme] = useState('bg-purple')
+  const [mapTheme, setMapTheme] = useState(
+    localStorage.getItem("mapTheme") || "mapbox://styles/mapbox/dark-v11"
+  );
+  const [colorTheme, setColorTheme] = useState("bg-purple");
 
-    const [isRegistering, setIsRegistering] = useState(false);
-    const [loggedIn, setLoggedIn] = useState(false);
-    const [settingsUserPassword, setSettingsUserPassword] = useState('');
-    const [settingsUsername, setSettingsUsername] = useState('');
-    const [jsWebToken, setJsWebToken] = useState('');
+  const [isRegistering, setIsRegistering] = useState(false);
+  const [loggedIn, setLoggedIn] = useState(false);
+  const [settingsUserPassword, setSettingsUserPassword] = useState("");
+  const [settingsUsername, setSettingsUsername] = useState("");
+  const [jsWebToken, setJsWebToken] = useState("");
 
-    return ( 
-    <AppContext.Provider value = {
-            {
-                showLogin,
-                setShowLogin,
-                showAddRecipe,
-                setShowAddRecipe,
-                showSettings,
-                setShowSettings,
-                showNotepad,
-                setShowNotepad,
+  return (
+    <AppContext.Provider
+      value={{
+        showLogin,
+        setShowLogin,
+        showAddRecipe,
+        setShowAddRecipe,
+        showSettings,
+        setShowSettings,
+        showNotepad,
+        setShowNotepad,
 
-                selectedCountry,
-                setSelectedCountry,
-                searchedRecipe,
-                setSearchedRecipe,
-                searchSubmit,
-                setSearchSubmit,
+        selectedCountry,
+        setSelectedCountry,
+        searchedRecipe,
+        setSearchedRecipe,
+        searchSubmit,
+        setSearchSubmit,
 
-                mapTheme,
-                setMapTheme,
-                colorTheme,
-                setColorTheme,
+        mapTheme,
+        setMapTheme,
+        colorTheme,
+        setColorTheme,
 
-                loggedIn,
-                setLoggedIn,
-                isRegistering,
-                setIsRegistering,
-                jsWebToken,
-                setJsWebToken,
-                settingsUsername,
-                setSettingsUsername,
-                settingsUserPassword,
-                setSettingsUserPassword
-            }
-        } >
-        {
-            children
-        } 
+        loggedIn,
+        setLoggedIn,
+        isRegistering,
+        setIsRegistering,
+        jsWebToken,
+        setJsWebToken,
+        settingsUsername,
+        setSettingsUsername,
+        settingsUserPassword,
+        setSettingsUserPassword,
+      }}
+    >
+      {children}
     </AppContext.Provider>
-    )
-
-}
+  );
+};
 
 export const useAppContext = () => {
-    const context = useContext(AppContext);
-    if (context === undefined) {
-        throw new Error("Not wrapped in context")
-    }
-    return context;
-}
+  const context = useContext(AppContext);
+  if (context === undefined) {
+    throw new Error("Not wrapped in context");
+  }
+  return context;
+};
