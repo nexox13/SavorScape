f4b00472462dc2e80dec7e4d2bcb6ed78b8be181
leonie <leoley.lange@gmail.com>
2024-05-16 06:23:56 +0000

    Added jwt tokens

--- recipeApi/controller/logincontroller.js
+++ recipeApi/controller/logincontroller.js
@@ -2,13 +2,16 @@ const express = require('express');
 const User = require('../model/user.js');
 const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
+const dotenv = require("dotenv");
+dotenv.config();
 
+var privateKey = process.env.PRIVATE_KEY;
 var router = express.Router();
 
 //login
 router.post('/', async (req, res) => {
     const user = await User.findOne({ username: req.body.username });
-
+    
     const userAllowed = await bcrypt.compare(req.body.password, user.password);
 
     if (userAllowed) {
@@ -18,7 +21,7 @@ router.post('/', async (req, res) => {
             username: userObject.username,
             password: userObject.password
         };
-        const token = jwt.sign(payload, 'secret-key-shhhh');
+        const token = jwt.sign(payload, privateKey, { expiresIn: '1h' });
         res.status(200).json({ message: 'User logged in successfully 😍', token });
     } else {
         res.send('No user found or invalid password 🫨');
--- recipeApi/controller/recipecontroller.js
+++ recipeApi/controller/recipecontroller.js
@@ -1,7 +1,6 @@
 const express = require('express');
 const router = express.Router();
 const Recipe = require('../model/recipe');
-const keycloak = require('../config/keycloak.js').getKeycloak();
 
 router.get('/', async (req, res) => {
   try {
@@ -33,7 +32,7 @@ router.get('/:name', async (req, res) => {
 });
 
 // POST: Neues Rezept hinzufügen
-router.post('/', keycloak.protect(), async (req, res) => {
+router.post('/', async (req, res) => {
   const recipe = new Recipe({
     country: req.body.country,
     title: req.body.title,
@@ -53,7 +52,7 @@ router.post('/', keycloak.protect(), async (req, res) => {
 });
 
 // PUT: Rezept bearbeiten
-router.put('/:id', keycloak.protect(), async (req, res) => {
+router.put('/:id', async (req, res) => {
   try {
     const updatedRecipe = await Recipe.findByIdAndUpdate(req.params.id, req.body, { new: true });
     res.json(updatedRecipe);
@@ -63,7 +62,7 @@ router.put('/:id', keycloak.protect(), async (req, res) => {
 });
 
 // DELETE: Rezept löschen
-router.delete('/:id', keycloak.protect(), async (req, res) => {
+router.delete('/:id', async (req, res) => {
   try {
     await Recipe.findByIdAndDelete(req.params.id);
     res.json({ message: 'Rezept gelöscht 😍' });
